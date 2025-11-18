e6dc48680ad3c3ad9e918c058a4b623af1a25f86
VEER0402 <veerendrad1999@gmail.com>
2025-11-18 15:07:09 +0000

    check nodeHelath using set parameter

---
+++ shell-scripts/nodeHealthWith_set_parameter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+###################################################################
+#
+#
+#Author : Veerendra Dwivedi
+#
+#Date : 18-Nov-2025 : 08:27pm
+#
+#
+#this scripts outhputs the node health
+#
+#       Version : V1
+#
+#
+#
+################################################################
+
+
+	set -x #debug mode
+
+
+df -h
+
+free -g
+
+nproc
