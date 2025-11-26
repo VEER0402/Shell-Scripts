459d9535894521b08bea64277891259d0fa31ed5
VEER0402 <veerendrad1999@gmail.com>
2025-11-26 15:10:22 +0000

    ok

---
+++ shell-scripts/function.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+print_menu() {
+    echo "-----------------------------"
+    echo " AWS Automation Project Menu "
+    echo "-----------------------------"
+    echo "1. List S3 Buckets"
+    echo "2. List EC2 Instances"
+    echo "3. Exit"
+}
+print_menu
